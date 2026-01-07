c57680d207dc00e651d91d0d69ab80c03d9ba0f5
Marjolijn-v <mverspiek@gmail.com>
2026-01-07 12:16:38 +0100

    deel 2, opdracht 2

--- src/pages/allBlogs/allBlogs.jsx
+++ src/pages/allBlogs/allBlogs.jsx
@@ -1,6 +1,8 @@
 import './allBlogs.css'
 import posts from '../../constants/data.json';
 import {Link} from "react-router-dom";
+import axios from "axios";
+import {useEffect, useState} from "react";
 
 
 
@@ -8,26 +10,51 @@ import {Link} from "react-router-dom";
 function AllBlogs() {
     // console.log(posts);
     // console.log(posts.length);
+    const [allBlogposts, setAllBlogposts] = useState([]);
+    const [error, setError] = useState('');
 
+    async function fetchBlogpost() {
+        try {
+            const response = await axios.get('https://novi-backend-api-wgsgz.ondigitalocean.app/api/blogposts', {
+                headers: {
+                    'novi-education-project-id': '2767c1c3-13ff-45b7-a2b7-6870077651b3',
+                }
+            });
+            // console.log(response);
+            setAllBlogposts(response.data);
+            setError('');
 
+        } catch (error) {
+            console.error(error);
+            setError('Er is iets misgegaan bij het ophalen van de blogposts.');
+        }
+    }
+
+    useEffect(() => {
+        void fetchBlogpost();
+        console.log(fetchBlogpost);
+    }, []);
 
     return (
         <>
             <header>
-                <h1>Bekijk alle {posts.length} posts op het platform</h1>
+                <h1>Bekijk alle {allBlogposts.length} posts op het platform</h1>
             </header>
             <div>
+                {error ? <p>{error}</p> :
+                    <ul className="list-of-blogs">
+                        {allBlogposts && allBlogposts.map((post) => {
+                            return <li key={post.id} className="list-item">
+                                <article className="card">
+                                    <span className="card-title"><h3><Link to={`/blogs/${post.title}`} className="link-to-blog">{post.title}</Link></h3><p>({post?.author})</p></span>
+                                    <p>{post.comments} reacties - {post?.shares} keer gedeeld</p>
+                                </article>
+                            </li>
+                        })}
+                    </ul>
+                }
 
-                <ul className="list-of-blogs">
-                    {posts.map((post) => {
-                        return <li key={post.id} className="list-item">
-                            <article className="card">
-                                <span className="card-title"><h3><Link to={`/blogs/${post.title}`} className="link-to-blog">{post.title}</Link></h3><p>({post?.author})</p></span>
-                                <p>{post.comments} reacties - {post?.shares} keer gedeeld</p>
-                            </article>
-                        </li>
-                    })}
-                </ul>
+
             </div>
 
         </>
